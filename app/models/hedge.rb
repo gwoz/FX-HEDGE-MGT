8f614b0065973f32234e82203e854025e5a2379e
Mike Gwozdek <email": null>
2016-03-19 15:22:35 -0400

    Update associations

--- app/models/hedge.rb
+++ app/models/hedge.rb
@@ -1,5 +1,5 @@
 class Hedge < ActiveRecord::Base
-  has_and_belongs_to_many :exposures
+  belongs_to :exposure
   belongs_to :entity
 
   validates :hedge_amount_currency, :hedge_against, :hedge_against_currency,
